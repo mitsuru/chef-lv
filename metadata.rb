c9355e7b26dfdfe5d0349918b92c4930b6cda788
myu <hayasaka.mitsuru+myu@gmail.com>
2013-07-22 14:41:47 +0900

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'lv'
+maintainer       'mitsuru'
+maintainer_email 'hayasaka.mitsuru@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures lv'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: lv
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'lv' do
+  action :install
+end
